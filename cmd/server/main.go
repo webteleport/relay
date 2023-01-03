e9cbcce5fa1979cc0b67f82faf2d2494bdb368e9
navigaid <navigaid@gmail.com>
2023-01-03 16:07:28 +0800

    add cmd/server/main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/webteleport/server"
+
+	_ "github.com/webteleport/utils/hack/quic-go-disable-receive-buffer-warning"
+)
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	err := server.Run(os.Args[1:])
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
